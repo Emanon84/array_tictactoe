cd0343ff7ebeb67189a25c456a20da7639002103
Timmy Lundstrom <timmy.lundstrom@gmail.com>
2023-02-02 20:46:27 +0100

    Add project files.

---
+++ array_tictactoe/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace array_tictactoe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Create Board
+            board Board1 = new board();
+
+           // Track used squares
+            int[] usedVals = new int[] {};
+
+            string input = "";
+            do
+            {
+
+            do
+            {
+                Board1.GetBoard();
+                Console.WriteLine("Player {0}: Choose where to put your sign", Board1.ActivePlayer);
+                input = Console.ReadLine();
+                if (input.Length != 1 || input.Equals("0"))
+                {
+                    Console.Clear();
+                    Console.WriteLine("You must choose 1-9");
+                }
+                else
+                {
+                    if (!int.TryParse(input, out int value))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Value must be an integer!");
+                    }
+                    else if (usedVals.Contains(value))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("{0} has already been used. Try again.", value);
+                    }
+                    else
+                    {
+                        //Mark square
+                        Console.Clear();
+                        switch (value)
+                        {
+                            case 1:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[0, 0] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[0, 1] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[0, 2] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[1, 0] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[1, 1] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+                            case 6:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[1, 2] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+                            case 7:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[2, 0] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+                            case 8:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[2, 1] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+                            case 9:
+                                {
+                                    usedVals = usedVals.Append(value).ToArray();
+                                    Board1.BoardState[2, 2] = Board1.SetActivePlayerMark();
+                                    Board1.ChangePlayer();
+                                    break;
+                                }
+
+                            default:
+                                break;
+                        }
+
+
+                    }
+                }
+
+
+            } while (!board.Checker2(Board1.BoardState) && usedVals.Length < 9);
+
+            Board1.GetBoard();
+
+            if (board.Checker2(Board1.BoardState)){
+ 
+                Board1.ChangePlayer();
+                Console.WriteLine("Game over! A winner is player {0}!", Board1.ActivePlayer);
+
+            }
+            else
+            {
+                Console.WriteLine("Game over! It's a draw!");
+            }
+
+            Console.WriteLine("Pressa ny key to play again!");
+                Console.ReadLine();
+                Console.Clear();
+                Board1.ResetBoard();
+                //Array.Clear(usedVals, 0, usedVals.Length);
+                usedVals = new int[] { };
+
+            } while (true);
+        }
+    }
+}
---
+++ array_tictactoe/board.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_tictactoe
+{
+    internal class board
+    {
+
+        private int activePlayer = 1;
+
+        public int ActivePlayer {
+            get { return activePlayer; } 
+        
+        } 
+
+        private string[,] boardstate =
+{
+                {"1","2","3" },
+                {"4","5","6" },
+                {"7","8","9" } 
+            };
+
+        public string[,] BoardState
+        {
+            get { return boardstate; }
+            set { boardstate = value; }
+        }
+
+        public void ResetBoard()
+        {
+           string[,] boardstateClean =
+{
+                {"1","2","3" },
+                {"4","5","6" },
+                {"7","8","9" } 
+            };
+            boardstate = boardstateClean;
+    }
+
+        public void ChangePlayer()
+        {
+            if(activePlayer == 1)
+            {
+                activePlayer = 2;
+            }
+            else
+            { 
+                activePlayer = 1; 
+            }
+        }
+
+        public string SetActivePlayerMark()
+        {
+            if (activePlayer == 1)
+            {
+                return "X";
+            }
+            else
+            {
+                return "O";
+            }
+        }
+
+
+        public void GetBoard()
+        {
+            Console.WriteLine(
+                    @"   
+                       |   |
+                     {0} | {1} |  {2}
+                    ___|___|___
+                       |   |
+                     {3} | {4} |  {5}
+                    ___|___|___
+                       |   |   
+                     {6} | {7} |  {8}   
+                       |   |   "
+            , boardstate[0, 0], boardstate[0, 1], boardstate[0, 2], 
+              boardstate[1, 0], boardstate[1, 1], boardstate[1, 2], 
+              boardstate[2, 0], boardstate[2, 1], boardstate[2, 2]);
+
+        }
+
+
+        
+
+
+        public static bool Checker(string[,] board)
+        {
+
+            int counterX = 0;
+            int counterO = 0;
+
+            Console.WriteLine("Checking left to right");
+            //vertical left to right
+            for(int i = 0; i< board.GetLength(0); i++)
+            {
+                for(int j = 0; j<board.GetLength(1); j++)
+                {
+
+                    if (i == j)
+                    {
+                        if (board[i, j].Equals("X"))
+                        {
+                            counterX++;
+                        }
+                        else if (board[i, j].Equals("O"))
+                        {
+                            counterO++;
+                        }
+
+                        if(counterX == 3 || counterO == 3)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+            }
+
+            //vertical right to left
+            counterX = 0;
+            counterO = 0;
+            Console.WriteLine("Checking right to left");
+
+            for (int i = 0, j = 2; i < board.GetLength(0); i++, j--)
+            {
+
+                if (board[i, j].Equals("X"))
+                {
+                    counterX++;
+                }
+                else if (board[i, j].Equals("O"))
+                {
+                    counterO++;
+                }
+
+                if (counterX == 3 || counterO == 3)
+                {
+                    return true;
+                }
+
+            }
+
+            Console.WriteLine("Checking rows");
+            counterX = 0;
+            counterO = 0;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                //Console.WriteLine("loop1");
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                   //Console.WriteLine("loop2");
+
+                    //Console.WriteLine(board[i, j]);
+                    if (board[i, j].Equals("X"))
+                    {
+                        counterX++;
+                    }
+                    else if (board[i, j].Equals("O"))
+                    {
+                        counterO++;
+                    }
+
+                    if (counterX == 3 || counterO == 3)
+                    {
+                        //Console.WriteLine("Tre i rad!. nollar counters");
+                        return true;
+                    }
+
+                    if (j == 2)
+                    {
+                       // Console.WriteLine("Nollar counters");
+                        counterX = 0;
+                        counterO = 0;
+                    }
+
+
+                }
+            }
+
+
+            Console.WriteLine("Checking columns");
+            counterX = 0;
+            counterO = 0;
+
+            for (int i = 0; i < board.GetLength(1); i++)
+            {
+                //Console.WriteLine("loop1");
+                for (int j = 0; j < board.GetLength(0); j++)
+                {
+                    //Console.WriteLine("loop2");
+
+                    //Console.WriteLine(board[i, j]);
+                    if (board[j, i].Equals("X"))
+                    {
+                        counterX++;
+                    }
+                    else if (board[j, i].Equals("O"))
+                    {
+                        counterO++;
+                    }
+
+                    if (counterX == 3 || counterO == 3)
+                    {
+                        //Console.WriteLine("Tre i rad!. nollar counters");
+                        return true;
+                    }
+
+                    if (j == 2)
+                    {
+                        // Console.WriteLine("Nollar counters");
+                        counterX = 0;
+                        counterO = 0;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        public static bool Checker2(string[,] board)
+        {
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                //vertical
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    return true;
+                }
+                //horizontal
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    return true;
+                }
+
+                //Diagonal right to left
+
+                if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+                {
+                    return true;
+                }
+
+                //Diagonal left to right
+
+                if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+                {
+                    return true;
+                }
+
+
+            }
+
+
+            return false;
+        }
+
+
+    }
+}
